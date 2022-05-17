b42d5b68354e0df3dfbcdd21da971863cf74fef3
5440000 <5440000@gmail.com>
2022-05-17 03:27:08 +0200

    123

--- src/UseState.js
+++ src/UseState.js
@@ -3,20 +3,38 @@ import React, { useState } from "react";
 function UseState() {
   const [total, setTotal] = useState(0);
   function increment() {
-    setTotal(total + 1);
+    setTotal((counter) => {
+      return counter + 1;
+    });
   }
 
   function decrement() {
     setTotal(total - 1);
   }
 
+  const [state,setState] = useState({
+    title: "Заголовок",
+    id: Date.now()
+  })
+
+  function updateTitle () {
+      setState(prev =>{
+        return {
+        ...prev,
+        title: "New Заголовок"
+        }
+      })
+  }
   return (
     <div>
       <h1>Total:{total}</h1>
-      <div>
-        <button onClick={increment}>+1 count</button>
-        <button onClick={decrement}>-1 count</button>
-      </div>
+      <button onClick={increment}>+1 count</button>
+      <button onClick={decrement}>-1 count</button>
+      <pre>
+        {JSON.stringify(state,null,2)}
+      </pre>
+      <button onClick={updateTitle}>New</button>
+
     </div>
   );
 }
